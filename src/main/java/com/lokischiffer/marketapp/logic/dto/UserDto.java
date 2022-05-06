7bbed1853476866ded02748aeb7de211e63d812c
LokiSchiffer <norbertomosquera@usantotomas.edu.co>
2022-05-06 16:38:05 -0500

    Include builder annotation in dto classes

--- src/main/java/com/lokischiffer/marketapp/logic/dto/UserDto.java
+++ src/main/java/com/lokischiffer/marketapp/logic/dto/UserDto.java
@@ -1,6 +1,7 @@
 package com.lokischiffer.marketapp.logic.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -12,6 +13,7 @@ import javax.validation.constraints.Size;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class UserDto {
 
     @Email (message = "Only valid emails allowed")
