f3593db929ff83920edb40a6e730a822e8bec979
LokiSchiffer <norbertomosquera@usantotomas.edu.co>
2022-05-07 13:22:19 -0500

    Implement method changes to accept new dummyDb

--- src/main/java/com/lokischiffer/marketapp/logic/service/AbstractCheckoutService.java
+++ src/main/java/com/lokischiffer/marketapp/logic/service/AbstractCheckoutService.java
@@ -1,7 +1,7 @@
 package com.lokischiffer.marketapp.logic.service;
 
+import com.lokischiffer.marketapp.db.model.UserDb;
 import com.lokischiffer.marketapp.db.repository.DummyDB;
-import com.lokischiffer.marketapp.logic.dto.ProductDto;
 import com.lokischiffer.marketapp.logic.dto.UserDto;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -16,23 +16,44 @@ public abstract class AbstractCheckoutService<T extends UserDto> {
         } else if (!resource.getPassword().equalsIgnoreCase(dummyDB.userList.get(resource.getEmail()).getPassword())) {
             return null;
         } else {
-            return dummyDB.userList.get(resource.getEmail());
+            return createUserDto(dummyDB.userList.get(resource.getEmail()));
         }
 
     }
 
-    protected final ProductDto createInternal(ProductDto product, T user) {
-        if (loginInternal(user) == null) {
-            throw new NullPointerException("User not identified");
+//    protected final ProductDto createInternal(ProductDto product, T user) {
+//        if (loginInternal(user) == null) {
+//            throw new NullPointerException("User not identified");
+//        }
+//        if (!dummyDB.productList.containsKey(product.getId())) {
+//            throw new NullPointerException("Product not found");
+//        } else if (dummyDB.productList.get(product.getId()).getQuantity() < product.getQuantity()) {
+//            throw new IllegalArgumentException("You're trying to reserved a bigger quantity than");
+//        } else {
+//            ProductDto productDto = createUserDto(dummyDB.productList.get(product.getId()));
+//            productDto.setQuantity(product.getQuantity());
+//            return productDto;
+//        }
+//    }
+
+
+
+    private UserDb createUser(T userDto){
+        long id = 0L;
+        if (dummyDB.existsByEmail(userDto.getEmail())){
+            id = dummyDB.findByEmail(userDto.getEmail()).getId();
         }
-        if (!dummyDB.productList.containsKey(product.getId())) {
-            throw new NullPointerException("Product not found");
-        } else if (dummyDB.productList.get(product.getId()).getQuantity() < product.getQuantity()) {
-            throw new IllegalArgumentException("You're trying to reserved a bigger quantity than");
-        } else {
-            ProductDto productDto = dummyDB.productList.get(product.getId());
-            productDto.setQuantity(product.getQuantity());
-            return productDto;
-        }
+        return createUser(id, userDto);
+    }
+
+    private UserDb createUser(long id, T userDto){
+        UserDb user = new UserDb(userDto.getEmail(), userDto.getName(), userDto.getPassword());
+        if (id != 0)
+            user.setId(id);
+        return user;
+    }
+
+    private UserDto createUserDto(UserDb user) {
+        return new UserDto(user.getEmail(), user.getName(), user.getPassword());
     }
 }
