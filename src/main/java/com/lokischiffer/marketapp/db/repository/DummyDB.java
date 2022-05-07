7658abb92c95f00ba3c62b8a914aa0ee584ed31c
LokiSchiffer <norbertomosquera@usantotomas.edu.co>
2022-05-07 13:20:41 -0500

    Change models from dto to db

--- src/main/java/com/lokischiffer/marketapp/db/repository/DummyDB.java
+++ src/main/java/com/lokischiffer/marketapp/db/repository/DummyDB.java
@@ -1,9 +1,9 @@
 package com.lokischiffer.marketapp.db.repository;
 
-import com.lokischiffer.marketapp.logic.dto.PaymentMethodDto;
-import com.lokischiffer.marketapp.logic.dto.ProductDto;
-import com.lokischiffer.marketapp.logic.dto.UserAddressDto;
-import com.lokischiffer.marketapp.logic.dto.UserDto;
+import com.lokischiffer.marketapp.db.model.PaymentMethodDb;
+import com.lokischiffer.marketapp.db.model.ProductDb;
+import com.lokischiffer.marketapp.db.model.UserAddressDb;
+import com.lokischiffer.marketapp.db.model.UserDb;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
@@ -12,10 +12,10 @@ import java.util.Map;
 @Component
 public class DummyDB {
 
-    public Map<String, UserDto> userList;
-    public Map<Long, ProductDto> productList;
-    public Map<Long, UserAddressDto> addressList;
-    public Map<Long, PaymentMethodDto> paymentMethodList;
+    public Map<String, UserDb> userList;
+    public Map<Long, ProductDb> productList;
+    public Map<Long, UserAddressDb> addressList;
+    public Map<Long, PaymentMethodDb> paymentMethodList;
 
     public DummyDB() {
         userList = new HashMap<>();
@@ -29,47 +29,55 @@ public class DummyDB {
     }
 
     private void createUsersDb() {
-        UserDto user = UserDto.builder().email("loki@mail.com").name("Norberto Mosquera")
+        UserDb user = UserDb.builder().email("loki@mail.com").name("Norberto Mosquera")
                 .password("NorbertoMosquera").build();
         userList.put(user.getEmail(), user);
-        user = UserDto.builder().email("nicmoseli@mail.com").name("Nicolas Mosquera")
+        user = UserDb.builder().email("nicmoseli@mail.com").name("Nicolas Mosquera")
                 .password("nicmoseli890918").build();
         userList.put(user.getEmail(), user);
-        user = UserDto.builder().email("mariajose@mail.com").name("Maria Jose Mosquera")
+        user = UserDb.builder().email("mariajose@mail.com").name("Maria Jose Mosquera")
                 .password("MajitoMosquera").build();
         userList.put(user.getEmail(), user);
     }
 
     private void fillProductDb() {
-        ProductDto product = ProductDto.builder().id(1).name("Cookies").quantity(20).build();
+        ProductDb product = ProductDb.builder().id(1).name("Cookies").quantity(20).build();
         productList.put(product.getId(), product);
-        product = ProductDto.builder().id(2).name("Milk").quantity(30).build();
+        product = ProductDb.builder().id(2).name("Milk").quantity(30).build();
         productList.put(product.getId(), product);
-        product = ProductDto.builder().id(3).name("Eggs").quantity(10).build();
+        product = ProductDb.builder().id(3).name("Eggs").quantity(10).build();
         productList.put(product.getId(), product);
     }
 
     private void fillAddressesDb() {
-        UserAddressDto address = UserAddressDto.builder().id(1001).addressName("Main Street")
+        UserAddressDb address = UserAddressDb.builder().id(1001).addressName("Main Street")
                 .addressNumber(152).build();
         addressList.put(address.getId(), address);
-        address = UserAddressDto.builder().id(1002).addressName("Boulevard street")
+        address = UserAddressDb.builder().id(1002).addressName("Boulevard street")
                 .addressNumber(94).build();
         addressList.put(address.getId(), address);
-        address = UserAddressDto.builder().id(1003).addressName("42nd street")
+        address = UserAddressDb.builder().id(1003).addressName("42nd street")
                 .addressNumber(67).build();
         addressList.put(address.getId(), address);
     }
 
     private void fillPaymentList() {
-        PaymentMethodDto paymentMethod = PaymentMethodDto.builder().id(101).type("Credit card")
+        PaymentMethodDb paymentMethod = PaymentMethodDb.builder().id(101).type("Credit card")
                 .funds(5000).build();
         paymentMethodList.put(paymentMethod.getId(), paymentMethod);
-        paymentMethod = PaymentMethodDto.builder().id(102).type("Cash")
+        paymentMethod = PaymentMethodDb.builder().id(102).type("Cash")
                 .funds(10000).build();
         paymentMethodList.put(paymentMethod.getId(), paymentMethod);
-        paymentMethod = PaymentMethodDto.builder().id(103).type("Check")
+        paymentMethod = PaymentMethodDb.builder().id(103).type("Check")
                 .funds(2000).build();
         paymentMethodList.put(paymentMethod.getId(), paymentMethod);
+    }
+
+    public boolean existsByEmail(String email){
+        return userList.containsKey(email);
+    }
+
+    public UserDb findByEmail(String email) {
+        return userList.get(email);
     }
 }
