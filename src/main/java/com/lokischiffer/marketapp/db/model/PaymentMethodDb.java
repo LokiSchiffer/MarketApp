a0ec44afb0180eeacf9ad186cce208f37984e7b2
LokiSchiffer <norbertomosquera@usantotomas.edu.co>
2022-05-07 13:19:44 -0500

    Implement lombok annotations

--- src/main/java/com/lokischiffer/marketapp/db/model/PaymentMethodDb.java
+++ src/main/java/com/lokischiffer/marketapp/db/model/PaymentMethodDb.java
@@ -1,5 +1,10 @@
 package com.lokischiffer.marketapp.db.model;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class PaymentMethodDb {
 
     private long id;
--- src/main/java/com/lokischiffer/marketapp/db/model/UserDb.java
+++ src/main/java/com/lokischiffer/marketapp/db/model/UserDb.java
@@ -1,5 +1,10 @@
 package com.lokischiffer.marketapp.db.model;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class UserDb {
 
     private long id;
@@ -7,4 +12,9 @@ public class UserDb {
     private String name;
     private String password;
 
+    public UserDb(String email, String name, String password) {
+        this.email = email;
+        this.name = name;
+        this.password = password;
+    }
 }
