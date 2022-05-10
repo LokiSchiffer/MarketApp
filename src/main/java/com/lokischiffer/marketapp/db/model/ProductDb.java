7b25beec5124b91ec8a75bd9f749acd6feed1449
LokiSchiffer <norbertomosquera@usantotomas.edu.co>
2022-05-10 14:08:23 -0500

    Put persistence annotations in Productdb to work with db

--- src/main/java/com/lokischiffer/marketapp/db/model/ProductDb.java
+++ src/main/java/com/lokischiffer/marketapp/db/model/ProductDb.java
@@ -3,12 +3,24 @@ package com.lokischiffer.marketapp.db.model;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
 @Builder
+@Entity(name = "Product_info")
 public class ProductDb {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Product_id")
     private long id;
+
+    @Column(unique = true, nullable = false)
     private String name;
+
+    @Column(nullable = false)
     private int quantityInStock;
+
+    @Column
     private int reservedQuantity;
 }
