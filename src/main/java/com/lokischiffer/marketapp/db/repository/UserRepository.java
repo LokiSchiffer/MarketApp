90bb2f98384484ce149521bfed5ad88fb38a4814
LokiSchiffer <norbertomosquera@usantotomas.edu.co>
2022-05-10 14:03:05 -0500

    Correct class annotation to make it repository

--- src/main/java/com/lokischiffer/marketapp/db/repository/UserRepository.java
+++ src/main/java/com/lokischiffer/marketapp/db/repository/UserRepository.java
@@ -2,11 +2,11 @@ package com.lokischiffer.marketapp.db.repository;
 
 import com.lokischiffer.marketapp.db.model.UserDb;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@ResponseBody
+@Repository
 public interface UserRepository extends JpaRepository<UserDb, Long> {
 
     Optional<UserDb> findByEmail(String email);
