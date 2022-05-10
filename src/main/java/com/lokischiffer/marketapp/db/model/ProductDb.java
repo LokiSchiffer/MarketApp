ca110307d1599d19d3085d08ca286375f789cabc
LokiSchiffer <norbertomosquera@usantotomas.edu.co>
2022-05-10 14:37:28 -0500

    Include no args costructor and all arg constructor annotations in productdb class

--- src/main/java/com/lokischiffer/marketapp/db/model/ProductDb.java
+++ src/main/java/com/lokischiffer/marketapp/db/model/ProductDb.java
@@ -1,12 +1,16 @@
 package com.lokischiffer.marketapp.db.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity(name = "Product_info")
 public class ProductDb {
 
