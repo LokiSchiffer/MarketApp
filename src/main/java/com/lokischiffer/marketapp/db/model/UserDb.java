e8008fddd9aae9bd042b241576a22b8672948604
LokiSchiffer <norbertomosquera@usantotomas.edu.co>
2022-05-10 13:53:01 -0500

    Put persistence annotations in Userdb to work with db

--- src/main/java/com/lokischiffer/marketapp/db/model/UserDb.java
+++ src/main/java/com/lokischiffer/marketapp/db/model/UserDb.java
@@ -4,14 +4,26 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
 @Builder
 @AllArgsConstructor
+@Entity(name = "User_info")
 public class UserDb {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "User_id")
     private long id;
+    
+    @Column(unique = true, nullable = false)
     private String email;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false)
     private String password;
 
     public UserDb(String email, String name, String password) {
